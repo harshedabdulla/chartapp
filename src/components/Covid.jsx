eb36bd45349b8300ed61db432419d22a2ea0d228
harshedabdulla <harshed2004@gmail.com>
2023-03-29 03:06:36 +0530

    change

---
+++ src/components/Covid.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Covid() {
+  return (
+    <div className="my-12 mx-52 text-lg font-medium text-gray-800">
+      <h1 className="text-2xl font-bold mb-4">COVID-19 Details</h1>
+      <h2 className="text-lg font-bold mb-2">COVID-19 in India</h2>
+      <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere massa vitae risus efficitur convallis.</p>
+      <h2 className="text-lg font-bold mb-2">COVID-19 in Kerala</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere massa vitae risus efficitur convallis.</p>
+    </div>
+  );
+}
+
+export default Covid;
